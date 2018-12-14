fb3f3be316b23d30e5626123da9e52b50d0d816e
Utkarsha17 <44276122+Utkarsha17@users.noreply.github.com>
2018-12-14 09:57:49 -0600

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,24 +1,21 @@
-
-
-#Call Library Table
+##Call Library Table
 library("data.table")
 
-#Load data in Table 
-dT <- data.table::fread(input = "household_power_consumption.txt",na.strings="?")
+##Loading data in Table 
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", na.strings = "?")
 
-#Converting scientific values data from table 
-dT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+##Converting Date and Time
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
 
-#Converting Date and Time
-dT[, Date := lapply(.SD, as.Date, "%d/%m/%Y"), .SDcols = c("Date")]
+##Filtering data from the dates 2007-02-01 and 2007-02-02
+Cleandata <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
 
-#Filtering data from the dates 2007-02-01 and 2007-02-02
-CleandT <- dT[(Date >= "2007-02-01") & (Date <= "2007-02-02")]
+##Drawing Plot 
+attach(Cleandata) #Attaching data source
+hist(Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "Red")
 
-#Draw Plot 
-hist(CleandT[, Global_active_power], main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-
-#Save file
+##Saving file
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
 dev.off()
-
+detach(Cleandata) # Detaching data source
